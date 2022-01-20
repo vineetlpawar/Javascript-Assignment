8f8d70e263b367569f869eb9f327d2161dfc435e
vineetlpawar <vineetlpawar96@gmail.com>
2022-01-20 12:21:09 +0530

    Run-Callback

--- assignments/run_callback/run_callback.js
+++ assignments/run_callback/run_callback.js
@@ -1,5 +1,8 @@
 function RunCallback(a, b, cb) {
     // Write you code here, you need to add a and b, pass the result into callback function cb as argument return the result
+    var res=a+b;
+    var ans=cb(res);
+    return ans;
 }
 
 module.exports = RunCallback;
