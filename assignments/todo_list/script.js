1beaae8fbf09675310c8efeb7ba66c6acffd7b3e
vineetlpawar <vineetlpawar96@gmail.com>
2022-02-02 09:53:30 +0530

    todo_list_with_api

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,17 +1,54 @@
-var todobutton = document.querySelector('button');
-var inputfield = document.querySelector('input');
+let todos = [];
+var addToDoButton = document.querySelector('button');
 var todo = document.querySelector('.todos')
-var i = 0;
-todobutton.addEventListener("click", additem)
-function additem(){
-    var para = document.createElement('p')
-    para.setAttribute("key",i);
-    i = i + 1;
-    para.innerHTML = inputfield.value;
-    todo.appendChild(para);
-    inputfield.value="";
-    para.addEventListener("click", deleteitem)
-    function deleteitem(){
-        todo.removeChild(para)
+let drop = document.querySelector("#dropdown");
+
+addToDoButton.addEventListener("click", work);
+async function main() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const json = await response.json();
+        todos = json.slice(0, 10);
+        console.log(todos);
+        todos.forEach((todom,index)=>{
+            const newTodo = document.createElement('p');
+            newTodo.setAttribute('key', index);
+            newTodo.innerHTML = todom.title
+            todo.appendChild(newTodo);
+        })
+    }catch(e) {
+        console.log(e);
     }
 }
+
+function work(){
+    if(drop.value ==="list"){
+        main()
+    }else if(drop.value === "completed"){
+        completed()
+    }else if(drop.value === "pending"){
+        pending()
+    }
+}
+
+function completed(){
+    todo.innerHTML = ""
+    todos.filter(todom => todom.completed).forEach((todom,index)=>{
+        const newTodo = document.createElement('p')
+        newTodo.setAttribute('key',index)
+        newTodo.innerHTML = todom.title
+        todo.appendChild(newTodo);
+    })
+
+}
+
+function pending(){
+    todo.innerHTML = ""
+    todos.filter(todom => !todom.completed).forEach((todom,index)=>{
+        const newTodo = document.createElement('p')
+        newTodo.setAttribute('key',index)
+        newTodo.innerHTML = todom.title
+        todo.appendChild(newTodo);
+    })
+}
+main()
